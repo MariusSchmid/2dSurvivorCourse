617a15fc8ddcb29a922277eb6dc9b1bdcf000c5e
Marius Schmid <mafloschmid@aol.com>
2024-10-28 20:25:55 +0100

    added health and upgrades

--- scenes/manager/upgrade_manager.gd
+++ scenes/manager/upgrade_manager.gd
@@ -2,6 +2,8 @@ extends Node
 
 @export var upgrade_pool: Array[AbilityUpgrade]
 @export var experience_manager: Node
+@export var upgrade_screen_scene: PackedScene
+
 
 var current_upgrades = {
 
@@ -11,8 +13,24 @@ func _ready():
 	experience_manager.level_up.connect(on_level_up)
 	
 func on_level_up(current_level: int):
-	var chosen_upgrade = upgrade_pool.pick_random()
+	var chosen_upgrade = upgrade_pool.pick_random() as AbilityUpgrade
 	if null == chosen_upgrade:
 		return
+	var upgrade_screen_instance = upgrade_screen_scene.instantiate()
+	add_child(upgrade_screen_instance)
+	upgrade_screen_instance.set_ability_upgrades([chosen_upgrade] as Array[AbilityUpgrade])
+	upgrade_screen_instance.upgrade_selected.connect(on_upgrade_seleced)
 	
-	
+func apply_upgrade(upgrade: AbilityUpgrade):
+	var has_upgrade = current_upgrades.has(upgrade.id)
+	if !has_upgrade:
+		current_upgrades[upgrade.id] = {
+			"resource": upgrade,
+			"quantity": 1
+		}
+	else:
+		current_upgrades[upgrade.id]["quantity"] +=1
+	GameEvents.emit_ability_upgrade_added(upgrade, current_upgrades)
+
+func on_upgrade_seleced(upgrade: AbilityUpgrade):
+	apply_upgrade(upgrade)
