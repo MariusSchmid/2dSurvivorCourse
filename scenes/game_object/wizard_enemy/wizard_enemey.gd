33e799bf2e857d16ad26850c16db8755e95f4642
Marius Schmid <mafloschmid@aol.com>
2024-11-03 13:03:25 +0100

    spawning wizards and basic enemies

--- scenes/game_object/wizard_enemy/wizard_enemey.gd
+++ scenes/game_object/wizard_enemy/wizard_enemey.gd
@@ -1,8 +1,14 @@
 extends CharacterBody2D
 
-
 @onready var velocity_component = $VelocityComponent
+@onready var visuals: Node = $Visuals
 
 func _process(delta: float) -> void:
 	velocity_component.accelerate_to_player()
 	velocity_component.move(self)
+	
+	var move_sign = sign(velocity.x)
+	if move_sign != 0:
+		visuals.scale = Vector2(move_sign,1)
+	
+	
